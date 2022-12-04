eb056ca39ced139cd4060c15d2f5c039b0d2e706
ianfights <ianfights@gmail.com>
2022-12-03 22:26:38 -0500

    Implemented bulk add

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,10 +1,12 @@
 import { CommandInteractionOptionResolver, ActionRowBuilder, ButtonBuilder, ButtonStyle, PermissionFlagsBits } from "discord.js";
 import { client } from "..";
+import bulkAdd from "../commands/CodeManagement/bulkAdd";
 import { Event } from "../lib/structures/Event";
 import { ExtendedInteraction } from "../lib/typings/Command";
 import { click } from "./cryptoClick/click";
 import { openAccTicket } from "./ticketCreations/account/accTicket";
 import { openGoldTicket } from "./ticketCreations/gold/goldTicket";
+import { addBulk } from "./ticketCreations/membership/addBulk";
 import { openMembershipTicket } from "./ticketCreations/membership/membershipTicket";
 
 export default new Event("interactionCreate", (interaction) => {
@@ -25,7 +27,7 @@ export default new Event("interactionCreate", (interaction) => {
         switch (interaction.customId) {
             case 'ticketSelect':
                 //@ts-ignore
-                switch(interaction.values[0]){
+                switch (interaction.values[0]) {
                     case 'membershipCodes':
                         openMembershipTicket(interaction)
                         break;
@@ -41,44 +43,45 @@ export default new Event("interactionCreate", (interaction) => {
                 interaction.channel.delete();
                 break;
             case 'bulkAdd':
-                
-                break;
+                //@ts-ignore
+                // console.log(interaction.fields.fields.get('codes'))
+                addBulk(interaction)
 
-        }
+                }
         if (interaction.customId.includes('ticketClose', 0)) {
-            let user = interaction.customId.replace('ticketClose_', '');
-            //@ts-ignore
-            interaction.channel.permissionOverwrites.set([
-                {
-                    id: process.env.guildId,
-                    deny: [PermissionFlagsBits.ViewChannel],
-                },
-                // {
-                //     id: hostId,
-                //     allow: [Permissions.FLAGS.VIEW_CHANNEL]
-                // },
-                // {
-                //     id: '923825468015276033',
-                //     allow: [Permissions.FLAGS.VIEW_CHANNEL]
-                // }
-            ]);
-            //@ts-ignore
-            interaction.channel.setParent('997628545700479076')
-            //@ts-ignore
-            interaction.channel.setName(`closedTicket_${user}`)
+                    let user = interaction.customId.replace('ticketClose_', '');
+                    //@ts-ignore
+                    interaction.channel.permissionOverwrites.set([
+                        {
+                            id: process.env.guildId,
+                            deny: [PermissionFlagsBits.ViewChannel],
+                        },
+                        // {
+                        //     id: hostId,
+                        //     allow: [Permissions.FLAGS.VIEW_CHANNEL]
+                        // },
+                        // {
+                        //     id: '923825468015276033',
+                        //     allow: [Permissions.FLAGS.VIEW_CHANNEL]
+                        // }
+                    ]);
+                    //@ts-ignore
+                    interaction.channel.setParent('997628545700479076')
+                    //@ts-ignore
+                    interaction.channel.setName(`closedTicket_${user}`)
 
-            const row = new ActionRowBuilder()
-                .addComponents(
-                    new ButtonBuilder()
-                        .setCustomId('deleteTicket')
-                        .setLabel('Delete Ticket')
-                        .setStyle(ButtonStyle.Danger),
-                );
-            //@ts-ignore
-            client.channels.cache.get(interaction.channel.id).send({ content: 'Click the button below to delete this ticket', components: [row] });
-            interaction.deferUpdate();
+                    const row = new ActionRowBuilder()
+                        .addComponents(
+                            new ButtonBuilder()
+                                .setCustomId('deleteTicket')
+                                .setLabel('Delete Ticket')
+                                .setStyle(ButtonStyle.Danger),
+                        );
+                    //@ts-ignore
+                    client.channels.cache.get(interaction.channel.id).send({ content: 'Click the button below to delete this ticket', components: [row] });
+                    interaction.deferUpdate();
+
+                }
 
         }
-
-    }
-});
+    });
---
+++ src/events/ticketCreations/membership/addBulk.ts
@@ -0,0 +1,35 @@
+import { readJson } from "../../../util/readJson";
+import * as prettier from 'prettier';
+import * as fs from 'fs'
+
+export const addBulk = (interaction) => {
+    //@ts-ignore
+    const modalCodes = interaction.fields.fields.get('codes').value.split('\n');
+    readJson('./codes.json', (err, iCodes) => {
+        let codes = iCodes.codes.unused;
+        for (let i in modalCodes) {
+
+            console.log(modalCodes[i])
+            const length = 16
+            let codeData = modalCodes[i];
+
+            switch (length) {
+                case 16:
+                    codes['16_days'].push(codeData)
+                    break;
+            }
+
+
+            interaction.reply({ content: `Sucessfully added ${modalCodes.length} new codes to the database!`, ephemeral: true })
+
+
+        }
+        let finalCodes = iCodes;
+
+        delete finalCodes.codes.unused;
+        finalCodes.codes.unused = codes
+        finalCodes = JSON.stringify(finalCodes);
+        prettier.format(finalCodes, { parser: "json" })
+        fs.writeFileSync('./codes.json', finalCodes);
+    });
+} 
