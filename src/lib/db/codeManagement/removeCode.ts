166b41188b10de40f09cafb9e32c956d3a3c2b5b
POKEBLOX6 <ianfights@gmail.com>
2023-01-27 13:36:36 -0500

    Code removal system should be done still awaiting testing

--- src/lib/db/codeManagement/removeCode.ts
+++ src/lib/db/codeManagement/removeCode.ts
@@ -1,13 +1,19 @@
 import { Codes } from "../models/codes";
 
 export const removeCode = async (amount: number) => {
-    const code = await Codes.find({claimed: false});
+    const code = await Codes.find({ claimed: false });
     // console.log(await code)
 
-    for(let i = 0;i<amount;i++){
+    const userCodes = []
+
+    for (let i = 0; i < amount; i++) {
         // console.log(code[i].codeValue)
+        // Bal.updateOne({ userId: userId }, prevLevel, (err) => {
+        Codes.updateOne({ codeValue: code[i].codeValue }, { claimed: true });
+        userCodes.push(code[0].codeValue)
     }
-    
+
+    return userCodes;
 }
 
 
